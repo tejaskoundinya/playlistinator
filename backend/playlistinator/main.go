e4b79d2b9e30a3eaad065a136b8b50ecddf2e231
Tejas Koundinya <tejaskoundinya@gmail.com>
2025-03-26 19:27:52 -0700

    add spotify types

--- backend/playlistinator/main.go
+++ backend/playlistinator/main.go
@@ -23,4 +23,46 @@ func main() {
 	type LastFmRecentTracksResponse struct {
 		RecentTracks LastFmRecentTracks `json:"recenttracks"`
 	}
+
+	// Spotify types
+	type SpotifySongTrack struct {
+		Uri  string `json:"uri"`
+		Id   string `json:"id"`
+		Name string `json:"name"`
+	}
+
+	type SpotifySongItem struct {
+		Track    SpotifySongTrack `json:"track"`
+		PlayedAt string           `json:"played_at"`
+	}
+
+	type SpotifyCursors struct {
+		After  string `json:"after"`
+		Before string `json:"before"`
+	}
+
+	type SpotifyRecentSongResponse struct {
+		Items   []SpotifySongItem `json:"items"`
+		Next    string            `json:"next"`
+		Cursors SpotifyCursors    `json:"cursors"`
+		Limit   int               `json:"limit"`
+	}
+
+	type SpotifyAddItemRequest struct {
+		Uris []string
+	}
+
+	type SpotifySongsResponse struct {
+		Songs     []string
+		Timestamp []int
+		Before    string
+	}
+
+	type SpotifyGetTrackTracks struct {
+		Items []SpotifySongTrack `json:"items"`
+	}
+
+	type SpotifyGetTrackResponse struct {
+		Tracks SpotifyGetTrackTracks `json:"tracks"`
+	}
 }
